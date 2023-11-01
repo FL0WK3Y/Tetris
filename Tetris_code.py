727c5fe90d09fe225767c0777f546b9d4bf6a5a9
gregori <g.rodriguez24680@gmail.com>
2023-11-01 15:42:19 -0400

    i finished the tetris game

--- Tetris_code.py
+++ Tetris_code.py
@@ -16,25 +16,39 @@ blocks = [
 [[1, 2, 5, 8], [5, 6, 7, 8], [0, 3, 6, 7],[0, 1, 2, 3]], #<---the L_BLOCK  type 2    
 [[4, 6, 7, 8], [0, 3, 4, 6], [0, 1, 2, 4],[2, 4, 5, 8]] #<---the ONE_ON_THREE_BLOCK  
 ] 
+
+colors = [ 
+(250,0,90) , (90,250,120),(90,100,250), (150,58,100), (100, 50 ,200),(150,0,0)  ,(0,0,150)
+]
 class Block:
     def __init__(self,x,y) :
         self.x= x
         self.y= y
-        self.type = random.randint(0,6)
+        self.type =  random.randint(0,len(blocks)-1)
         self.rotation = 0
+        self.colors = colors[random.randint(0,len(colors)-1)]
     def shape(self):
       return blocks[self.type][self.rotation] 
 def draw_block():
     for y in range(3):
         for x in range(3) :
            # this is flattening the array and then it checks if it has the index inside of the block shape 
+           # rambow color block //pygame.draw.rect(screen,(random.randint(0,255),random.randint(0,255),random.randint(0,255))
            if y * 3 + x in block.shape():
-               pygame.draw.rect(screen,(80,55,55),
+               pygame.draw.rect(screen,block.colors,
                                 [(x+block.x)*grid_size + x_gap+1, (y + block.y) * grid_size + y_gap + 1, grid_size -2 , grid_size -2] )
 
 def rotate():
+    last_rotation = block.rotation 
     block.rotation = (block.rotation+ 1)% len(blocks[block.type])
-
+    can_rotate = True
+    for y in range(3):
+        for x in range(3):
+            if y * 3 + x in block.shape():
+                if collides(0,0):
+                    can_rotate = False
+    if not can_rotate:
+        block.rotation = last_rotation
 def draw_grid():
     for y in range(rows): 
         #this is to make the horizantal collums in the grid
@@ -45,14 +59,30 @@ def draw_grid():
             if game_board[x][y] !=(0,0,0):
                 pygame.draw.rect(screen,game_board[x][y], [x * grid_size + x_gap +1 , y * grid_size + y_gap +1 ,grid_size -1,grid_size -1])    
             
-            
+def collides(nx,ny):
+    collision =False
+    for y in range(3):
+        for x in range(3):
+            if y * 3 + x in block.shape():
+                if x +block.x + nx <0 or x +block.x +nx > cols -1:
+                    collision = True
+                    break
+                if y +block.y + ny <0 or y +block.y +ny > rows -1:
+                    collision = True
+                    break
+                if game_board[x+ block.x +nx][y+block.y +ny] != (0,0,0):
+                    collision= True
+                    break 
+    return collision
+
+
 # this function drops the block to the bottom of the grid and stops it 
 def drop_block():
     can_drop =True
     for y in range(3):
         for x in range(3):
             if y * 3 + x in block.shape():
-                if block.y +y >= rows - 1:
+                if collides(0,1):
                     can_drop = False
     if can_drop:
         block.y += 1
@@ -60,7 +90,7 @@ def drop_block():
         for y in range(3):
            for x in range(3):
                if y * 3 + x in block.shape():
-                   game_board[x + block.x][y+block.y]= (0,255,0)
+                   game_board[x + block.x][y+block.y]= block.colors
     return can_drop
                    
 #this funtion make the left and right borders and does not let you go passed them  
@@ -69,20 +99,31 @@ def side_move(dx):
     for y in range(3):
         for x in range(3):
             if y * 3 + x in block.shape():
-                if x +block.x >=cols -1 and dx == 1:
+                if collides(dx,0):
                     can_move=False
-                elif x+block.x< 1 and dx == -1:
-                    can_move=False
+                
     if can_move:                
         block.x +=dx
     else: 
         drop_block()
-
-
+# This code is to delete a line of blocks. once the game board have no more black spots in a line on the x axis then it get rid of that line
+def find_lines():
+    lines = 0
+    for y in range(rows):
+        empty = 0
+        for x in range (cols):
+            if game_board[x][y] ==(0,0,0):
+              empty += 1
+        if empty == 0:
+            lines += 1
+            for y2 in range(y,1,-1):
+                for x2 in range(cols):
+                    game_board[x2][y2] = game_board[x2][y2 - 1]
+    return lines
 
 grid_size = 30 
 pygame.init()
-screen = pygame.display.set_mode((800,400))
+screen = pygame.display.set_mode((400,800))
 rows = screen.get_height() //grid_size
 cols = screen.get_width()// grid_size
 x_gap = (screen.get_width()- cols * grid_size)//2
@@ -102,7 +143,12 @@ for i in range(cols):
    game_board.append(new_col)
    for j in range(rows):   
        new_col.append((0,0,0))
-
+score = 0
+font = pygame.font.SysFont('Arial',35, True, False)
+font2 = pygame.font.SysFont('Arial',70, True, False)
+finished_text = font2.render("Game Over ", True, (255,255,255),(1,1,1))
+game_finished = False
+ftpos = ((screen.get_width()-finished_text.get_width())//2,(screen.get_height()-finished_text.get_height())//2)
 # this is the game loop for tetris
 while not game_over:
     clock.tick(fps)
@@ -129,8 +175,18 @@ while not game_over:
         draw_block()
     #this event make the "gravity stop when you hold the left and right keys  "
         if event.type != pygame.KEYDOWN:
-            if not drop_block():
-                block = Block(random.randint(5,cols -5),0)
-            
+            if not drop_block() and not game_finished:
+                score += find_lines()
+                block = Block(random.randint(2,cols -5),0)
+                if collides(0,0):
+                    game_finished = True
+    
+    # this code displays and updates the score
+    text = font.render("Score: " + str(score), True , (2,2,2),(150,150,150) ) 
+    if game_finished == True:
+        screen.blit(finished_text,ftpos)
+    
+    screen.blit(text ,[0,0]) 
     pygame.display.update()
+    
 pygame.quit()
